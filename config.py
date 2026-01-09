acb6b21463d39e185d3ffb6b58ad6bf591c56c4a
Arianne <76917463+ariannelafraise@users.noreply.github.com>
2026-01-09 01:44:08 -0500

    renamed plugins directory to modules

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class PathConfig:
     RULES_FILE_PATH = "/etc/audit/rules.d/honeypot.rules"
     LOG_FILE_PATH = "/var/log/audit/audit.log"
     CWD = os.getcwd()
-    PLUGINS_DIR = "/home/arianne/personal_dev/hived/plugins"
-    HANDLERS_DIR = "/home/arianne/personal_dev/hived/plugins"
+    PLUGINS_DIR = "/home/arianne/personal_dev/hived/modules"
+    HANDLERS_DIR = "/home/arianne/personal_dev/hived/modules"
