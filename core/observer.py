f9ba1cce00d595926a98c4ea788824d498dd0c97
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-01-10 14:48:12 -0500

    fixed wrong import

--- core/observer.py
+++ core/observer.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 
 from core.audit_event import AuditEvent
-from utils.import_utils import import_event_handlers
+from core.modules import import_event_handlers
 
 
 class AuditEventDispatcher(ABC):
