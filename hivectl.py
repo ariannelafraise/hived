978adc343e2af109498d7640bbf4a448242cd37c
Arianne <76917463+ariannelafraise@users.noreply.github.com>
2026-06-17 19:30:05 -0400

    app registeration from hivectl

--- hivectl.py
+++ hivectl.py
@@ -1,13 +1,37 @@
 import argparse
+from pathlib import Path
 
 from core.external import import_plugins
 from __version__ import __version__
+
+def absolute_directory(path: str) -> Path:
+    """
+    Validate that the given path is an absolute path to an existing directory.
+    """
+    p = Path(path)
+
+    if not p.is_absolute():
+        raise argparse.ArgumentTypeError("must be an absolute path")
+
+    if not p.exists():
+        raise argparse.ArgumentTypeError("directory does not exist")
+
+    if not p.is_dir():
+        raise argparse.ArgumentTypeError("path is not a directory")
+
+    return path
+
+def register_application(path: str):
+    with open("/etc/hivesec/apps", "a") as file:
+        file.write(path)
 
 def handle_command(arguments: argparse.Namespace):
     """
     Handling command of the args parser.
     """
-    pass
+    if arguments.register:
+        register_application(arguments.register)
+        return
 
 
 if __name__ == "__main__":
@@ -25,6 +49,13 @@ if __name__ == "__main__":
         help="View currently installed version.",
         action="version",
         version=f"v{__version__}",
+    )
+    group.add_argument(
+        "-r",
+        "--register",
+        metavar="DIR",
+        type=absolute_directory,
+        help="Register an HiveSec application.",
     )
 
     args = parser.parse_args()
